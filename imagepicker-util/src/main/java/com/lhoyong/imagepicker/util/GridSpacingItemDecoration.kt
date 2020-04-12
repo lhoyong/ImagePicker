07d751ff11b46f3a2040bdd96f6da7d3df11575f
leehoyong <lee199402@gmail.com>
2020-04-12 20:39:39 +0900

    Add util module

--- imagepicker-util/src/main/java/com/lhoyong/imagepicker/util/GridSpacingItemDecoration.kt
+++ imagepicker-util/src/main/java/com/lhoyong/imagepicker/util/GridSpacingItemDecoration.kt
@@ -4,7 +4,7 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-internal class GridSpacingItemDecoration(
+class GridSpacingItemDecoration(
     private val spanCount: Int,
     private val spacing: Int,
     private val includeEdge: Boolean
